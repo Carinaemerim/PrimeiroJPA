020a11b54a4bed88dca07cca84928d2644e1093a
Aluno <Aluno@200.17.90.11>
2017-10-04 13:48:34 -0300

    asdfsdf

--- jpaapp/src/main/java/br/edu/ifrs/canoas/jee/jpaapp/dao/UsuarioDAO.java
+++ jpaapp/src/main/java/br/edu/ifrs/canoas/jee/jpaapp/dao/UsuarioDAO.java
@@ -1,5 +1,7 @@
 package br.edu.ifrs.canoas.jee.jpaapp.dao;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -13,29 +15,61 @@ public class UsuarioDAO {
     private EntityManager em;
 
     public UsuarioDAO() {
+    	
     }
 
     public void salva(Usuario usuario) {
+    	em = EntityManagerUtil.getEM();
+		em.getTransaction().begin();
+		em.persist(usuario);
+		em.getTransaction().commit();
+		em.close();
+    	
     }
 
     public void atualiza(Usuario usuario) {
+    			
+    	em = EntityManagerUtil.getEM();
+    	em.getTransaction().begin();
+    	em.merge(usuario);
+    	em.getTransaction().commit();
+    	em.close();
+    	
     }
 
     public void remove(Long id) {
+    	em = EntityManagerUtil.getEM();
+    	em.getTransaction().begin();
+    	em.remove(id);
+    	em.getTransaction().commit();
+    	em.close();
+    	
     }
 
     public Usuario busca(Long id) {
-    	return null;
+    	em = EntityManagerUtil.getEM();
+    	Usuario usuario;
+    	usuario = em.find(Usuario.class, id);
+    	em.close();
+    	return usuario;
 
     }
 
     public List<Usuario> busca() {
-    	return null;
-
+    	em = EntityManagerUtil.getEM();
+    	TypedQuery<Usuario> query = em.createQuery("select usr from Usuario usr", Usuario.class);
+    	em.close();
+    	return query.getResultList();
     }
 
     public List<Usuario> buscaPorEmail(String email) {
     	
-    	return null;
+        em = EntityManagerUtil.getEM();
+    	
+    	TypedQuery<Usuario> query = em.createQuery("select usr from Usuario where usr.email = :email", Usuario.class);
+    	query.setParameter("email", email);
+    	em.close();
+    	return query.getResultList();
     }
+    
 }
